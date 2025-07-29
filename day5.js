d1bd6757e1b39b7a23d5a716b5383355a41b9991
bharath26122006 <24bct109@kgcas.com>
2025-07-29 10:51:09 +0530

    Add files via upload

---
+++ day5.js
@@ -0,0 +1,12 @@
+let number = 9944294580
+let test=/^[6-9]\d{9}$/;
+if(value.test (enter.toString())){
+    console.log("correct")
+}
+else{
+      console.log("check your valid num")
+}
+
+let text="hai hello";
+let words=text.match(/\ba\w+e\b/gi);
+console.log(words);
